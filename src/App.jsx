6eef926462806c98359b2fc3cc87c292b5c3d548
Renato Gouveia <rpgouveia@gmail.com>
2023-02-11 16:18:32 -0300

    feat: add login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import ApplicationRoutes from './routes/ApplicationRoutes'
+import { useNavigate } from 'react-router-dom'
+import { api } from './services/api.js'
+import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
+  // States
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
 
+  // Callbacks
+  const loginUser = async (formData) => {
+    try {
+      const response = await api.post('/sessions', formData)
+      const token = response.data.token
+      const userId = response.data.user.id
+      const userData = response.data.user
+      setUser(userData)
+      localStorage.setItem('@TOKEN', JSON.stringify(token))
+      localStorage.setItem('@USERID', JSON.stringify(userId))
+      toast.success('Login feito com sucesso')
+      navigate('/home')
+    } catch (error) {
+      console.log(error)
+      toast.error('Email ou senha inválido')
+    }
+  }
+  
   return (
-    <div>
-      <ApplicationRoutes />
-    </div>
+    <>
+      <ApplicationRoutes loginUser={loginUser} />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   )
 }
 
---
+++ src/components/Logo/styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledLogo = styled.img`
+    height: 14px;
+    width: 101px;
+    object-fit: cover;
+`
+
+export default StyledLogo
